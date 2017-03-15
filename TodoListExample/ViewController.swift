e689aa25a0e21dc5eaff82bfe9da295c06e36007
KeiKawano <keikaw12@gmail.com>
2017-03-16 01:30:39 +0900

    ダイアログを表示してTODO内容をUITableViewに追加。

--- TodoListExample/ViewController.swift
+++ TodoListExample/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var todoList = [String]()
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapAddButton(_ sender: Any) {
+        // アラートダイアログを生成
+        let alertController = UIAlertController(title: "TODO追加", message: "TODOを入力してください", preferredStyle: UIAlertControllerStyle.alert)
+        // テキストエリアを追加
+        alertController.addTextField(configurationHandler: nil)
+        // OKボタンを追加
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default){(action:UIAlertAction) in
+            //ボタンがタップされたときの処理
+            if let textField = alertController.textFields?.first {
+                //ToDoの配列に入力値を挿入（先頭に挿入する）
+                self.todoList.insert(textField.text!, at: 0)
+                // テーブルに行が追加されたことをテーブルに通知
+                self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableViewRowAnimation.right)
+            }
+        }
+        // OKボタンを追加
+        alertController.addAction(okAction)
+        
+        // CANCELボタンがタップされたときの処理
+        let cancelButton = UIAlertAction(title: "CANCEL", style: UIAlertActionStyle.cancel, handler: nil)
+        // CANCELボタンを追加
+        alertController.addAction(cancelButton)
+        
+        // アラートダイアログを表示
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // テーブルの行数を返却する
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Todoの配列の長さを返却する
+        return todoList.count
+    }
+    
+    // テーブルの行ごとのセルを返却する
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Storyboadで設定したトドCell識別子を利用して再利用可能なセルを取得する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: <#T##IndexPath#>)
+        // 行番号にあったToDoのタイトルを取得
+        let todoTitle = todoList[indexPath.row]
+        // セルのラベルにToDoのタイトルをセット
+        cell.textLabel?.text = todoTitle
+        return cell
+    }
 
 }
 
