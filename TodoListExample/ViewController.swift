9beb4bfec34e6a0294fa28db1314b140ec292123
KeiKawano <keikaw12@gmail.com>
2017-03-16 01:39:55 +0900

    bug fix -> editor placeholder in source file

--- TodoListExample/ViewController.swift
+++ TodoListExample/ViewController.swift
@@ -59,7 +59,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // テーブルの行ごとのセルを返却する
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Storyboadで設定したトドCell識別子を利用して再利用可能なセルを取得する
-        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: <#T##IndexPath#>)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
         // 行番号にあったToDoのタイトルを取得
         let todoTitle = todoList[indexPath.row]
         // セルのラベルにToDoのタイトルをセット
